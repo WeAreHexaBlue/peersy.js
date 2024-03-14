f3029435f1f0fc8c7c4c46bd5f1eea67b6bbf9f7
ActuallyFlamey <morellialex08@gmail.com>
2024-03-14 10:29:30 +0100

    and requesting too whatttt

--- peersy/peer.ts
+++ peersy/peer.ts
@@ -13,6 +13,8 @@ export class Peer {
     content: peersy.Content[]
     partialContent: peersy.PartialContent[]
 
+    forbidDisconnect: boolean
+
     constructor(platform: peersy.Platform) {
         this.emitter = new events.EventEmitter()
 
@@ -34,6 +36,8 @@ export class Peer {
         this.#privateKey = privateKey
 
         this.#getLocalContent()
+
+        this.forbidDisconnect = false
 
         peersy.connectedPeers.push(this)
 
@@ -60,11 +64,56 @@ export class Peer {
                 packets.push(packet)
             })
 
-            packets.forEach(packet => {
-                this.emitter.emit(`${magnet}:send`, packet)
+            this.emitter.once(`${magnet}:ready`, () => {
+                packets.forEach(packet => {
+                    this.emitter.emit(`${magnet}:send`, packet)
+                })
             })
 
             this.emitter.once(`${magnet}:get`, () => {})
+        })
+
+        this.emitter.on("upload", (contentID: number, magnet: string) => {
+            this.emitter.emit(`${magnet}:me`, this)
+
+            this.emitter.once(`${magnet}:info`, (length: number) => {
+                this.partialContent.push({
+                    id: contentID,
+                    length: length,
+                    packets: []
+                })
+
+                this.emitter.emit(`${magnet}:ready`)
+            })
+
+            this.emitter.on(`${magnet}:send`, (packet: peersy.Packet) => {
+                let contentAt = this.partialContent.findIndex(partialContent => {partialContent.id === contentID})
+
+                this.partialContent[contentAt].packets.push(packet)
+
+                if (this.partialContent[contentAt].packets.length === this.partialContent[contentAt].length) {
+                    this.emitter.removeAllListeners(`${magnet}:send`)
+
+                    let data: string
+
+                    let decryptedPackets: peersy.Packet[] = []
+                    this.partialContent[contentAt].packets.forEach((packet, index) => {
+                        packet.content = crypto.privateDecrypt(this.#privateKey, Buffer.from(packet.content, "base64url")).toString()
+                        decryptedPackets[index] = packet
+                    })
+
+                    data = decryptedPackets.join("")
+
+                    this.content.push({
+                        id: contentID,
+                        data: data
+                    })
+
+                    this.partialContent.splice(contentAt, 1)
+
+                    this.emitter.emit(`${magnet}:get`)
+                }
+            })
         })
     }
 
@@ -73,7 +122,7 @@ export class Peer {
             throw peersy.BlacklistedContent
         }
 
-        let magnet = crypto.randomBytes(8).toString("hex")
+        let magnet = crypto.randomBytes(8).toString("base64url")
 
         this.emitter.once(`${magnet}:info`, (length: number) => {
             this.partialContent.push({
@@ -81,6 +130,8 @@ export class Peer {
                 length: length,
                 packets: []
             })
+
+            this.emitter.emit(`${magnet}:ready`)
         })
 
         this.emitter.on(`${magnet}:send`, (packet: peersy.Packet) => {
@@ -113,6 +164,48 @@ export class Peer {
         })
 
         this.emitter.emit("req", this, contentID, magnet)
+    }
+
+    async upload(content: peersy.Content) {
+        if (content.id in peersy.blacklist) {
+            throw peersy.BlacklistedContent
+        }
+
+        this.forbidDisconnect = true
+
+        let magnet = crypto.randomBytes(8).toString("base64url")
+
+        let transactions = 0
+        this.emitter.on(`${magnet}:me`, (requester: peersy.Peer) => {
+            let encryptedContent: string = crypto.publicEncrypt(requester.publicKey, Buffer.from(content.data)).toString("base64url")
+            let contentChunks = encryptedContent.match(/.{1,40}/g)
+
+            if (!contentChunks) {return}
+
+            this.emitter.emit(`${magnet}:info`, contentChunks.length)
+
+            let packets: peersy.Packet[] = []
+            contentChunks.forEach((chunk, index) => {
+                let packet: peersy.Packet = {index: index, content: chunk}
+                packets.push(packet)
+            })
+
+            this.emitter.once(`${magnet}:ready`, () => {
+                packets.forEach(packet => {
+                    this.emitter.emit(`${magnet}:send`, packet)
+                })
+            })
+
+            this.emitter.once(`${magnet}:get`, () => {
+                transactions++
+            })
+
+            if (transactions >= 5 && this.forbidDisconnect) {
+                this.forbidDisconnect = false
+            }
+        })
+
+        this.emitter.emit("upload", content.id, magnet)
     }
 
     #getLocalContent() {
